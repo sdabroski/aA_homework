6e565ae0398ed9cc29d666c248604a9e3dfd473a
Stefan Dabroski <stefan@wave.com>
2019-04-04 22:04:47 -0700

    W4D4 HW

---
+++ W4D4/music_app copy/app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+class ApplicationController < ActionController::Base
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def current_user
+        User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?(user)
+        current_user.session_token == session[:session_token]
+    end
+
+    def logout!(user)
+        # user.reset_session_token!
+
+    end
+end
